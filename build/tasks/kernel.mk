b02cc81e2539f4c0070433634068c2e5cff3090d
Nick Reuter <nreuter85@gmail.com>
2022-11-26 15:49:31 -0600

    dragonboard: initial evervolv setup
    Change-Id: Ic8b04c391c68a62c62c890f1c08a80405f1982cf

--- build/tasks/kernel.mk
+++ build/tasks/kernel.mk
@@ -1,8 +1,12 @@
+ifeq ($(wildcard vendor/*/build/tasks/kernel.mk),)
+
 ifneq ($(filter db845c db845c_mini, $(TARGET_DEVICE)),)
 
 $(PRODUCT_OUT)/dtb.img: $(TARGET_KERNEL_DIR)/sdm845-db845c.dtb $(wildcard $(TARGET_KERNEL_DIR)/qrb5165-rb5.dtb)
 	cat $^ > $@
 
 droidcore: $(PRODUCT_OUT)/dtb.img
+
+endif
 
 endif
---
+++ rb5/BoardConfigEvervolv.mk
@@ -0,0 +1,10 @@
+#
+# Product-specific compile-time definitions.
+#
+include device/linaro/dragonboard/BoardConfigEvervolv.mk
+
+BOARD_KERNEL_IMAGE_NAME := Image.gz
+BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat device/linaro/dragonboard/modules.load))
+TARGET_KERNEL_CONFIG := gki_defconfig
+KERNEL_CONFIG_OVERRIDE := $(strip $(shell cat arch/$(TARGET_ARCH)/configs/db845c_gki.fragment))
+TARGET_KERNEL_DTB := qrb5165-rb5.dtb
