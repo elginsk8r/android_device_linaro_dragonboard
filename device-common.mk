b896566c1059a34b50e6c2081c5231caa4434c97
Nick Reuter <nreuter85@gmail.com>
2022-11-28 00:25:45 -0600

    dragonboard: add linux-firmware extract tools
    Change-Id: I008fb54f6995b11745716c27ed65b54ea543afa7

--- device-common.mk
+++ device-common.mk
@@ -34,7 +34,9 @@ PRODUCT_SHIPPING_API_LEVEL := 33
 # please modify the source git project here:
 #   https://staging-git.codelinaro.org/linaro/linaro-aosp/aosp-linaro-vendor-package
 include device/linaro/dragonboard/vendor-package-ver.mk
-ifneq (,$(wildcard $(LINARO_VENDOR_PATH)/db845c/$(EXPECTED_LINARO_VENDOR_VERSION)/version.mk))
+ifneq (,$(wildcard $(LINARO_VENDOR_PATH)/dragonboard/dragonboard-vendor.mk))
+    $(call inherit-product, $(LINARO_VENDOR_PATH)/dragonboard/dragonboard-vendor.mk)
+else ifneq (,$(wildcard $(LINARO_VENDOR_PATH)/db845c/$(EXPECTED_LINARO_VENDOR_VERSION)/version.mk))
   # Unfortunately inherit-product doesn't export build variables from the
   # called make file to the caller, so we have to include it directly here.
   include $(LINARO_VENDOR_PATH)/db845c/$(EXPECTED_LINARO_VENDOR_VERSION)/version.mk
@@ -44,6 +46,11 @@ ifneq (,$(wildcard $(LINARO_VENDOR_PATH)/db845c/$(EXPECTED_LINARO_VENDOR_VERSION
     $(warning    ./device/linaro/dragonboard/fetch-vendor-package.sh )
     # Would be good to error out here, but that causes other issues
   endif
+
+  PRODUCT_SOONG_NAMESPACES += \
+    vendor/linaro/linux-firmware/$(EXPECTED_LINARO_VENDOR_VERSION) \
+    vendor/linaro/db845c/$(EXPECTED_LINARO_VENDOR_VERSION) \
+    vendor/linaro/rb5/$(EXPECTED_LINARO_VENDOR_VERSION)
 else
   $(warning Missing Linaro Vendor Package!)
   $(warning Please download and extract the vendor binaries by running the following script:)
@@ -52,10 +59,7 @@ else
 endif
 
 PRODUCT_SOONG_NAMESPACES += \
-    device/linaro/dragonboard \
-    vendor/linaro/linux-firmware/$(EXPECTED_LINARO_VENDOR_VERSION) \
-    vendor/linaro/db845c/$(EXPECTED_LINARO_VENDOR_VERSION) \
-    vendor/linaro/rb5/$(EXPECTED_LINARO_VENDOR_VERSION)
+    device/linaro/dragonboard
 
 # Dynamic partitions
 PRODUCT_BUILD_SUPER_PARTITION := true
